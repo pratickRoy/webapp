df81fcf748479bab3d54eaf8c0386f4326f38bae
Pratick Roy <praticr@amazon.com>
2020-08-15 14:32:40 +0530

    Updates

---
+++ precache-manifest.4e14f01eb38fd880ab0c94547c91d49e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3093e0a9295ef5690e58dff2b404742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a06355496eb00e6fd0",
+    "url": "/static/css/2.41fc952c.chunk.css"
+  },
+  {
+    "revision": "cf7652611cb344828320",
+    "url": "/static/css/main.f17d278a.chunk.css"
+  },
+  {
+    "revision": "f3a06355496eb00e6fd0",
+    "url": "/static/js/2.5731d429.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5731d429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7652611cb344828320",
+    "url": "/static/js/main.3f084a12.chunk.js"
+  },
+  {
+    "revision": "78f72dff81760e2673af",
+    "url": "/static/js/runtime-main.91b7d7c2.js"
+  }
+]);
