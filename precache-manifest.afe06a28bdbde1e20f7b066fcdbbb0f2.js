93d53ac4c9d1e76058f9eb4f51819120cbff4a64
Pratick Roy <praticr@amazon.com>
2020-09-30 18:05:14 +0530

    Updates

---
+++ precache-manifest.afe06a28bdbde1e20f7b066fcdbbb0f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f001004f65c2ac0a3b2e33f501c70dae",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "1b2ad7355830492ceb5c",
+    "url": "/webapp/static/css/2.375b92aa.chunk.css"
+  },
+  {
+    "revision": "0ca445759615d2ba3595",
+    "url": "/webapp/static/css/main.6c445d09.chunk.css"
+  },
+  {
+    "revision": "1b2ad7355830492ceb5c",
+    "url": "/webapp/static/js/2.1b6ab191.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/webapp/static/js/2.1b6ab191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca445759615d2ba3595",
+    "url": "/webapp/static/js/main.3a76a199.chunk.js"
+  },
+  {
+    "revision": "4536633d59aa3e4f59a5",
+    "url": "/webapp/static/js/runtime-main.f91bfdd2.js"
+  },
+  {
+    "revision": "77b1587d053ae91a8b9c844b9f5f4476",
+    "url": "/webapp/static/media/are-void-methods-bad-feature-image.77b1587d.png"
+  },
+  {
+    "revision": "9c271fad4ba98f060a5648ce361ba6fa",
+    "url": "/webapp/static/media/dots-feature-image.9c271fad.png"
+  },
+  {
+    "revision": "ddf01aa63e83f4477ab9beef75933e7d",
+    "url": "/webapp/static/media/landing-fragment-background-image.ddf01aa6.jpg"
+  },
+  {
+    "revision": "707345ef49af0561e0e7fd212370bed8",
+    "url": "/webapp/static/media/landing-fragment-display-image.707345ef.jpg"
+  },
+  {
+    "revision": "7c85f7acf3f462b71277d9b5766ad7e5",
+    "url": "/webapp/static/media/ml-feature-image.7c85f7ac.png"
+  },
+  {
+    "revision": "aca0b6ae1f7c445d3821e8b1662ec199",
+    "url": "/webapp/static/media/tictactoe-feature-image.aca0b6ae.png"
+  },
+  {
+    "revision": "8a3e30da15b8a5e3b3dd71a454c80bb7",
+    "url": "/webapp/static/media/what-is-good-code-feature-image.8a3e30da.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc34d6fc83e830747f9871843b1b380b.js"
+  "/webapp/precache-manifest.afe06a28bdbde1e20f7b066fcdbbb0f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
